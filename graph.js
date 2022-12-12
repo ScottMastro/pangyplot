b31832ae6e4956a482d51049e7aa214862ba71eb
scott <scott@skyridge.sickkids.ca>
2022-12-12 17:20:54 -0500

    parser in

--- graph.js
+++ graph.js
@@ -1,7 +1,38 @@
-fetch('/home/scott/projects/graph_viz/graph_viz/data')
-  .then(response => response.text())
-  .then(text => console.log(text))
+function parseTabSeparatedLayout(tsv) {
+    let nodes = [];
+    let i = 0;
+    d3.tsv(tsv, function(data) {
+        if (i % 2 == 0){
+            nodes.push([[parseFloat(data.X), parseFloat(data.Y)]]);
+        } else {
+            nodes[nodes.length - 1].push([parseFloat(data.X), parseFloat(data.Y)]);
+        }    
+        i = i+1;
+    });
 
-d3.tsv("/home/scott/projects/graph_viz/graph_viz/data", function(data) {
-    // use data here
-});
+    return nodes;
+
+}
+
+let nodes = parseTabSeparatedLayout("https://raw.githubusercontent.com/ScottMastro/example_graph_data/master/data/DRB1-3123_sorted.lay.tsv")
+//console.log(nodes);
+
+function parseGfa(gfa) {
+    let edges = [];
+
+    d3.tsv(gfa, function(data) {
+        if (data["#0"] == "L"){
+            let n1 = parseInt(data["1"]);
+            let n2 = parseInt(data["3"]);
+            
+            let node_coord1=nodes[n1-1][1];
+            let node_coord2=nodes[n2-1][0];
+                
+            edges.push([node_coord1, node_coord2, n1,n2])
+        }
+    });
+    return edges
+}
+
+let edges = parseGfa("https://raw.githubusercontent.com/ScottMastro/example_graph_data/master/data/DRB1-3123.gfa")
+console.log(edges);
