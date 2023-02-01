c05aafbcc58f6c8d7dc5f94ccef99ffa9df443f0
scott <scott@skyridge.sickkids.ca>
2023-02-01 17:46:28 -0500

    highlight chromosome

--- static/javascript/cytoband.js
+++ static/javascript/cytoband.js
@@ -10,21 +10,53 @@ function get_longest_size(data) {
     return longest;
 }
 
+function highlight_genome_chr(rectangle, info) {
+
+    let rectangles = document.getElementsByClassName("chromosome-selection-genome");
+    for (let i = 0; i < rectangles.length; i++) {
+        rectangles[i].classList.remove("chromosome-selection-highlight");
+    }
+
+    rectangle["target"].classList.add("chromosome-selection-highlight");
+
+    let annotations = document.getElementsByClassName("annotation-genome-chr");
+    annotations = annotations[0].firstElementChild.childNodes;
+
+    for (let i = 0; i < annotations.length; i++) {
+        let content = annotations[i].childNodes[2].childNodes[0];
+        let bg = content.childNodes[0];
+        let label = content.childNodes[1].childNodes[0];
+
+        bg.classList.remove("annotation-highlight");
+        label.classList.remove("annotation-highlight-label");
+
+        if(label.textContent == info["chr"]){
+            bg.classList.add("annotation-highlight");
+            label.classList.add("annotation-highlight-label");
+        }
+
+    }
+}
+
+
 function draw_genome_(data) {
 
     let width = 1000;
-    let height = 200;
-    var shift = 5;
+    let height = 400;
+    var shift = 8;
 
     let chr_width = 10;
-    let gap = 15;
-    let scale = height - (shift * 2);
+    let gap = 20;
+    let scale = 200;
+    let chr_height = scale - 2*shift;
 
     var svg = d3.select("#genome")
 	.append("svg")
 	.attr("width", width)
 	.attr("height", height);
     
+    const annotations = []
+
     let longest = get_longest_size(data);
     let i = 1;
     for (const [key, value] of Object.entries(data)) {
@@ -39,23 +71,49 @@ function draw_genome_(data) {
             .attr("rx", 4)
             .attr("ry", 4)
             .attr("width", chr_width+shift*2)
-            .attr("height", scale + shift*2)
-            .attr("fill", "#222222");
+            .attr("height", scale)
+            .attr("class", "chromosome-selection-genome")
+            .on('click', function(d,i) {
+                highlight_genome_chr(d,i);
+            });
 
         var rects = svg.selectAll("foo")
             .data(value)
             .enter()
             .append("rect")
             .attr("x", x_)
-            .attr("y", d=> scale*(d.end/longest) + shift)
+            .attr("y", d=> chr_height*(d.start/longest) + shift)
             .attr("width", chr_width)
-            .attr("height", d=> ((d.end-d.start)/longest *scale) )
-            .attr("fill", d=> d.color);
+            .attr("height", d=> ((d.end-d.start)/longest *chr_height) )
+            .attr("fill", d=> d.color)
+            .attr("class", "chromosome-band");
         
+        var adjust = 0;
+        if (i % 2 == 0){
+            adjust = 20;
+        }
+        annotations.push(
+            {note: {label: key, bgPadding: 3},
+            x: x_ + chr_width/2,
+            y: scale,
+            dy: 10 + adjust,
+            className: "annotation-genome-chr-bg",
+            dx: 0,
+            });
 
         i=i+1;
     }
+    
+    const makeAnnotations = d3.annotation()
+        .type(d3.annotationLabel)
+        .annotations(annotations);
 
+    d3.select("#genome")
+        .select("svg")
+        .append("g")
+        .attr("class", "annotation-group")
+        .attr("class", "annotation-genome-chr")
+        .call(makeAnnotations);
 
 }
 
