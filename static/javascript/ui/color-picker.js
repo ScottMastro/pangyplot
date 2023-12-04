784d118bc7f65cc4b6b1464af3d1b205672ab8ff
scott <scott@skyridge.sickkids.ca>
2023-12-04 18:04:53 -0500

    update css, color picker

---
+++ static/javascript/ui/color-picker.js
@@ -0,0 +1,48 @@
+function applyGradient(color1, color2, color3) {
+    let gradient;
+    let useMiddle = true;
+    if (color1 == color2 || color3 == color2){
+        useMiddle = false;
+    }
+    if (useMiddle) {
+        gradient = `linear-gradient(to right, ${color1}, ${color2}, ${color3})`;
+    } else {
+        gradient = `linear-gradient(to right, ${color1}, ${color3})`;
+    }
+    document.getElementById('gradient-display').style.background = gradient;
+}
+
+document.querySelectorAll('.color-picker').forEach(picker => {
+    picker.addEventListener('change', () => {
+        const color1 = document.getElementById('color1').value;
+        const color2 = document.getElementById('color2').value;
+        const color3 = document.getElementById('color3').value;
+        unselectAllButtons("color-picker-container")
+        applyGradient(color1, color2, color3);
+    });
+});
+
+function updateColorPickers(color1, color2, color3) {
+    document.getElementById('color1').value = color1;
+    document.getElementById('color2').value = color2;
+    document.getElementById('color3').value = color3;
+}
+document.querySelectorAll('.preset-color').forEach(elem => {
+    elem.addEventListener('click', () => {
+        const color1 = elem.getAttribute('data-color1');
+        const color2 = elem.getAttribute('data-color2');
+        const color3 = elem.getAttribute('data-color3');
+        updateColorPickers(color1, color2, color3);
+        applyGradient(color1, color2, color3);
+    });
+});
+
+let defaultChoice = document.getElementsByClassName('preset-color option-button-selected')[0];
+if (defaultChoice) {
+    let color1 = defaultChoice.getAttribute('data-color1');
+    let color2 = defaultChoice.getAttribute('data-color2');
+    let color3 = defaultChoice.getAttribute('data-color3');
+
+    applyGradient(color1, color2, color3);
+    updateColorPickers(color1, color2, color3);
+}
---
+++ static/javascript/ui/option-button.js
@@ -0,0 +1,28 @@
+function unselectAllButtons(containerId){
+    document.querySelectorAll('#' + containerId + ' .option-button').forEach(button => {
+        button.classList.remove('option-button-selected');
+        button.classList.add('option-button-unselected');
+    });
+}
+
+function setupButtonSelectionLogic(containerId) {
+  document.getElementById(containerId).addEventListener('click', function(event) {
+      if (event.target.classList.contains('option-button') || event.target.parentNode.classList.contains('option-button')) {
+          // Remove 'selected' class from all buttons in the container
+          unselectAllButtons(containerId)
+
+          // Add 'selected' class to clicked button
+          let targetButton = event.target.classList.contains('option-button') ? event.target : event.target.parentNode;
+          targetButton.classList.add('option-button-selected');
+          targetButton.classList.remove('option-button-unselected');
+      }
+  });
+}
+
+
+
+// Apply the logic to the color-option-button-container
+setupButtonSelectionLogic('color-style-container');
+
+// Apply the same logic to the color-picker-container
+setupButtonSelectionLogic('color-picker-container');
