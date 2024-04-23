5e439f74560571d2514d09d028edbb72747be701
ScottMastro <scott-mastro@hotmail.com>
2024-04-23 03:17:42 -0400

    trying to get a refpos to work

--- db/query/query_top_level.py
+++ db/query/query_top_level.py
@@ -3,75 +3,57 @@ import db.utils.create_record as record
 import db.utils.integrity_check as integrity
 
 
-def get_top_level_aggregates(db, session, genome, chrom, start, end):
+def get_top_level_data(db, session, genome, chrom, start, end):
     nodes,links = [],[]
 
     query = """
-            MATCH (n:Chain|Bubble)
+            MATCH (n:Segment|Chain|Bubble)
             WHERE n.db = $db AND n.genome = $genome AND n.chrom = $chrom 
                 AND n.start <= $end AND n.end >= $start AND NOT EXISTS {
                     MATCH (n)-[:INSIDE|PARENT]->(m)
                     WHERE m.chrom = $chrom AND m.start <= $end AND m.end >= $start
             }
-            OPTIONAL MATCH (n)-[r:END]-(e:Segment)
-            RETURN n, labels(n) AS type, collect(DISTINCT r) AS links
+            OPTIONAL MATCH (n)-[r1:END]-(e:Segment)
+            OPTIONAL MATCH (n)-[r2:LINKS_TO]-(s:Segment)
+            OPTIONAL MATCH (n)-[r3:END]-(a:Chain|Bubble)
+
+            RETURN n, labels(n) AS type,
+             a, labels(a) AS type2,
+            collect(DISTINCT r1) AS endlinks,
+            collect(DISTINCT r2) AS links
             """
     parameters = {"start": start, "end": end, "db": db, "genome": genome, "chrom": chrom}
     results = session.run(query, parameters)
 
     for result in results:
         nodes.append( record.node_record(result["n"], result["type"][0]) )
+        if("a" in result):
+            print(result["type2"])
+            nodes.append( record.node_record(result["a"], result["type2"][0]) )
+
+        links.extend( [record.link_record_simple(r) for r in result["endlinks"]] )
         links.extend( [record.link_record_simple(r) for r in result["links"]] )
 
     return nodes, links
-
-def get_top_level_segments(db, session, genome, chrom, start, end):
-    nodes,links = [],[]
-
-    query = """
-            MATCH (n:Segment)
-            WHERE n.db = $db AND n.genome = $genome AND n.chrom = $chrom 
-                AND n.start <= $end AND n.end >= $start AND NOT EXISTS {
-                    MATCH (n)-[:INSIDE|PARENT]->(m)
-                    WHERE m.chrom = $chrom AND m.start <= $end AND m.end >= $start
-            }
-            OPTIONAL MATCH (n)-[r:LINKS_TO]-(s:Segment)
-            RETURN n, s, collect(DISTINCT r) AS links
-            """
-
-    parameters = {"start": start, "end": end, "db": db, "genome": genome, "chrom": chrom}
-    results = session.run(query, parameters)
-
-    collected_nodes = set()
-    for result in results:
-        node = record.segment_record(result["n"])
-        if node["nodeid"] not in collected_nodes:
-            nodes.append(node)
-            collected_nodes.add(node["nodeid"])
-
-        links.extend( [record.link_record_simple(r) for r in result["links"]] )
-
-    return nodes, links
-
 
 def get_top_level(genome, chrom, start, end):
 
     with get_session() as (db, session):
-        aggregates,aggregateLinks = get_top_level_aggregates(db, session, genome, chrom, start, end)
-        print(len(aggregates))
+        nodes,links = get_top_level_data(db, session, genome, chrom, start, end)
+        print(len(nodes))
 
-        segments,segmentLinks = get_top_level_segments(db, session, genome, chrom, start, end)
+        #segments,segmentLinks = get_top_level_segments(db, session, genome, chrom, start, end)
 
-    nodes = aggregates + segments
-    links = aggregateLinks + segmentLinks
+    #nodes = aggregates + segments
+    #links = aggregateLinks + segmentLinks
 
     nodes = integrity.deduplicate_nodes(nodes)
     links = integrity.deduplicate_links(links)
     links = integrity.remove_invalid_links(nodes, links)
 
     print(f"TOP LEVEL QUERY: {chrom}:{start}-{end}")
-    print(f"   Nodes: C+B={len(aggregateLinks)}, S={len(segments)}")
-    print(f"   Links: C+B={len(aggregateLinks)}, S={len(segmentLinks)}")
+    print(f"   Nodes:{len(nodes)}")
+    print(f"   Links:{len(links)}")
     
     graph = {"nodes": nodes, "links": links}
 
