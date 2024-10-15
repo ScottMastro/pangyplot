c42b1ab94125dd0d057a9af4949180b73d981706
ScottMastro <scott-mastro@hotmail.com>
2024-10-15 03:17:00 -0400

    adjust examples

--- static/js/ui/navbar.js
+++ static/js/ui/navbar.js
@@ -1,7 +1,7 @@
 document.getElementById('navbar-button-example').addEventListener('click', function() {
 
     const data = {
-        genome: "CHM13",
+        genome: "GRCh38",
         chrom: "chr18",
         start: 47506000,
         end: 47600000,
@@ -12,13 +12,22 @@ document.getElementById('navbar-button-example').addEventListener('click', funct
 
 document.getElementById('navbar-button-example2').addEventListener('click', function() {
 
+    //SERPINB5
     const data = {
-        genome: "CHM13",
-        chrom: "chrM",
-        start: 1000,
-        end: 10000,
+        genome: "GRCh38",
+        chrom: "chr18",
+        start: 63476958-10000,
+        end: 63505085+10000,
         source:"navbar-example"
     };
+
+    //const data = {
+    //    genome: "CHM13",
+    //    chrom: "chrM",
+    //    start: 1000,
+    //    end: 10000,
+    //    source:"navbar-example"
+    //};
 
     document.dispatchEvent(new CustomEvent('selectedCoordinatesChanged', { detail: data }));
 
@@ -26,14 +35,24 @@ document.getElementById('navbar-button-example2').addEventListener('click', func
 
 document.getElementById('navbar-button-example3').addEventListener('click', function() {
 
+    //const data = {
+    //    genome: "CHM13",
+    //    chrom: "XXX",
+    //    start: -1000,
+    //    end: 999999999999,
+    //    source:"navbar-example"
+    //};
+    
+    //SMAD4
     const data = {
-        genome: "CHM13",
-        chrom: "XXX",
-        start: -1000,
-        end: 999999999999,
+        genome: "GRCh38",
+        chrom: "chr18",
+        start: 51028528-10000,
+        end: 51085045+10000,
         source:"navbar-example"
     };
-    
+
+
     document.dispatchEvent(new CustomEvent('selectedCoordinatesChanged', { detail: data }));
 
 });
