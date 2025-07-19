8afbbebe0e739d5dd112cfd5eb6e02c5c7bc58fb
ScottMastro <scott-mastro@hotmail.com>
2025-07-19 04:40:28 -0400

    restructure (delet)

--- static/js/ui/tabs/tool-tab.js
+++
@@ -1,7 +0,0 @@
-function toolHandleAnchorToggle(isChecked) {
-    const event = new CustomEvent('tool-setting-changed', {
-        detail: { type: "anchor", value: isChecked }
-      });
-    window.dispatchEvent(event);
-}
-
